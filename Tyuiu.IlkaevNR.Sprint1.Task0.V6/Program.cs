4030e72e2bcad663c697ff45773aa768c6567237
Ilkaev Nail <ilkaevnail3@gmail.com>
2026-09-26 15:53:54 +0500

    Task0

---
+++ Tyuiu.IlkaevNR.Sprint1.Task0.V6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.IlkaevNR.Sprint1.Task0.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
