467b99ef491386786987ee4155fc9ac4a145fd0b
Ilkaev Nail <ilkaevnail3@gmail.com>
2026-09-26 16:53:44 +0500

    Task0

---
+++ Tyuiu.IlkaevNR.Sprint1.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.IlkaevNR.Sprint1.Task0.V6.Lib;
+
+namespace Tyuiu.IlkaevNR.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(15, res);
+        }
+    }
+}
--- Tyuiu.IlkaevNR.Sprint1.Task0.V6/Program.cs
+++ Tyuiu.IlkaevNR.Sprint1.Task0.V6/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Tyuiu.IlkaevNR.Sprint1.Task0.V6
+﻿using Tyuiu.IlkaevNR.Sprint1.Task0.V6.Lib;
+
+namespace Tyuiu.IlkaevNR.Sprint1.Task0.V6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Илькаев Н. Р. | ИСТНб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Илькаев Н. Р. | ИСТНб-26-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 3 * (4 + 1)             *");
+            Console.WriteLine("* и печатает результат на экране                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 3 * (4 + 1)                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
         }
     }
 }
