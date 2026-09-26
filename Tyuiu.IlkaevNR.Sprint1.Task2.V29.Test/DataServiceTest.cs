02fed4e374fad07f58e3a346a67d2faa1fcbcf0f
Ilkaev Nail <ilkaevnail3@gmail.com>
2026-09-26 18:49:41 +0500

    Task2

---
+++ Tyuiu.IlkaevNR.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.IlkaevNR.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.IlkaevNR.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 3600;
+            var res = ds.ConvertSecondsToHours(x);
+            Assert.AreEqual(1, res);
+
+        }
+    }
+}
---
+++ Tyuiu.IlkaevNR.Sprint1.Task2.V29/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.IlkaevNR.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.IlkaevNR.Sprint1.Task2.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Илькаев Н. Р. | ИСТНб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #29                                                              *");
+            Console.WriteLine("* Выполнил: Илькаев Н. Р. | ИСТНб-26-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 3 * (4 + 1)             *");
+            Console.WriteLine("* и печатает результат на экране                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение x (в секундах):");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество часов в указанных секундах:" + ds.ConvertSecondsToHours(x));
+
+            Console.ReadLine();
+        }
+    }
+}
