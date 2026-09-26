93f4b8be583a97b885ff892fab02695d742dcf2f
Ilkaev Nail <ilkaevnail3@gmail.com>
2026-09-26 15:57:28 +0500

    Task0

---
+++ Tyuiu.IlkaevNR.Sprint1.Task0.V6.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.IlkaevNR.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
